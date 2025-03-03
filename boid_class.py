2e7f2c8843219b995559c178b02e2c91532b477e
Oscar <osgun7841@uit.no>
2025-03-03 17:02:44 +0100

    Added Hoik

--- boid_class.py
+++ boid_class.py
@@ -1,17 +1,13 @@
 import pygame
 from abc import ABC, abstractmethod
 import random
-import time
-import math
 
 pygame.init()
 
 # Global Settings
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-SPEED = 0.5
 NUMBER_OF_BOIDS = 50
-SIGHT_RANGE = 10
 
 # Abstract class for all enteties
 class Enteties(ABC):
@@ -21,20 +17,41 @@ class Enteties(ABC):
     def update(self):
         pass
 
+    # Make the Boid steer towards other Boids nearby
+    @abstractmethod
+    def cohesion(self):
+        pass
+
+    # Make the Boid steer towards the average Heading of Boids nearby
+    @abstractmethod
+    def alignment(self):
+        pass
+
+    # Make the Boid steer away from other Boids nearby
+    @abstractmethod
+    def seperation(self):
+        pass
+
     # Draw the object
     @abstractmethod
     def draw(self):
         pass
 
-class Boid(Enteties, pygame.sprite.Sprite):
+class Boid(Enteties):
 
     def __init__(self, x ,y):
         image = pygame.image.load('Boid.png')
         self.image = pygame.transform.scale(image, (42,30))
+
+        # Setting
+        self.sight_range = 60
+        self.speed = 0.5
+
         # Start with a random position
         self.position = pygame.math.Vector2(x,y)
-        self.velocity = pygame.math.Vector2(random.uniform(-1,1), random.uniform(-1,1)).normalize() * SPEED
+        self.velocity = pygame.math.Vector2(random.uniform(-1,1), random.uniform(-1,1)).normalize() * self.speed
         self.boids = []
+    
 
     def update(self, boids):
         self.boids = boids
@@ -70,9 +87,9 @@ class Boid(Enteties, pygame.sprite.Sprite):
         cohesion = cohesion - self.position
 
         # Adding cohesion force
-        steeringvelocity = (pygame.math.Vector2(cohesion.x , cohesion.y).normalize() * SPEED) - self.velocity
-        self.velocity += steeringvelocity * 0.01
-        self.velocity = self.velocity.normalize() * SPEED
+        steeringvelocity = (pygame.math.Vector2(cohesion.x , cohesion.y).normalize() * self.speed) - self.velocity
+        self.velocity =  (self.velocity + steeringvelocity * 0.01).normalize() * self.speed
+        #self.velocity = self.velocity.normalize() * self.speed
 
     def alignment(self):
         alignment = pygame.math.Vector2(0,0)
@@ -83,57 +100,75 @@ class Boid(Enteties, pygame.sprite.Sprite):
         alignment = alignment - self.velocity
 
         # Adding alignment force
-        self.velocity += alignment *0.01
-        self.velocity = self.velocity.normalize() * SPEED
+        self.velocity = (self.velocity + alignment *0.01).normalize() * self.speed
+        #self.velocity = self.velocity.normalize() * self.speed
 
     def seperation(self):
         # Calculating and applying seperation force by distance
         # The seperation force only applies if the distance to a boid is 
         # less than half the Sight Range
         for b in self.boids:
-            if (self.position.distance_to(b.position) < (SIGHT_RANGE / 1.5)):
-                seperation_force = 1 / self.position.distance_to(b.position) 
-                seperation_vector = (self.position - b.position) / 2
-                self.velocity += seperation_vector * seperation_force
-                self.velocity = self.velocity.normalize() * SPEED
+            if (self.position.distance_to(b.position) < (self.sight_range / 4)):
+                seperation_force = 0.5 / self.position.distance_to(b.position) 
+                seperation_vector = (self.position - b.position) 
+                self.velocity = (self.velocity + seperation_vector * seperation_force) * self.speed
+                #self.velocity = self.velocity.normalize() * self.speed
 
     def draw (self, screen):
         self.update(self.boids)
         screen.blit(self.image, (self.position.x, self.position.y))
 
 class Hoik(Boid):
-    pass
+     
+    def __init__(self, x ,y):
+        #Inherit from Boid class
+        super().__init__(x,y)
+        image = pygame.image.load('Hoik.png')
+        self.image = pygame.transform.scale(image, (42,30))
+        self.sight_range = 100
+        self.speed = 0.3
 
+    # Since the Hoik should not avoid Boids, seperation is not needed
+    def seperation(self):
+        pass
+    
 # Controls all the boids
 class Flock():
 
     def __init__(self):
         self.boids = [Boid(random.randrange(0, SCREEN_WIDTH), random.randrange(0,SCREEN_HEIGHT)) for _ in range(NUMBER_OF_BOIDS)]
-
+        self.boids.append(Hoik(random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)))
+        self.dead_boids = []
 
     def collision(self):
-        for i in range(NUMBER_OF_BOIDS):
+        for i in range(len(self.boids)):
             nearby_boids = []
-            too_close_boids = []
-            for j in range(NUMBER_OF_BOIDS):
+            for j in range(len(self.boids) - 1):
                 # Boid can not collide with itself
                 if( i != j ):
-                    if(self.boids[j].position.distance_to(self.boids[i].position)) < 60:
+                    if(self.boids[j].position.distance_to(self.boids[i].position)) < self.boids[i].sight_range:
                         nearby_boids.append(self.boids[j])
 
-                    if(self.boids[j].position.distance_to(self.boids[i].position)) < 30:
-                        too_close_boids.append(self.boids[j])
+                if((i == len(self.boids) - 1)):
+                    if(self.boids[len(self.boids)- 1].position.distance_to(self.boids[j].position) < 10):
+                        if self.boids[j] in self.boids:
+                            print("object is in list")
+                        self.dead_boids.append(self.boids[j])
             
             if nearby_boids:
                 self.boids[i].update(nearby_boids)
 
- 
+        if self.dead_boids:
+            for d in self.dead_boids:
+                self.boids.remove(d)
+
+            self.dead_boids = []
+
     def update_boids(self, screen):
         self.collision()
-        for i in range(NUMBER_OF_BOIDS):
+        for i in range(len(self.boids)):
             self.boids[i].draw(screen)
 
-    
 running = True
 
 display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -141,8 +176,11 @@ clock = pygame.time.Clock()
 flock = Flock()
 
 while(running):
-    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
     display.fill((0,0,0))
     flock.update_boids(display)
     pygame.display.flip()
-
